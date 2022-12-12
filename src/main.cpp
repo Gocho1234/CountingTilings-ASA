151092a65c79efc2fdbd9f36c0a6f6ad1f733d3e
Gocho1234 <goncalo.barias@gmail.com>
2022-12-12 02:33:13 +0000

    Added new vector hashing function and got 1500 in mooshak (2 memory limits, T22 and T23).

--- src/main.cpp
+++ src/main.cpp
@@ -6,74 +6,86 @@
  */
 
 #include <algorithm>
+#include <cstring>
 #include <iostream>
 #include <unordered_map>
 #include <vector>
 
 typedef unsigned long long ull;
 
-ull solve(std::vector<int> *stair);
-int get_top_right(std::vector<int> *stair);
-int get_max_tiling_size(int top_right, std::vector<int> *stair);
-std::vector<int> copy_vector(std::vector<int> *stair);
-std::string generate_state(std::vector<int> *stair);
+struct vector_hasher {
+    int operator()(const std::vector<int> &v) const {
+        int hash = v.size();
+        for (auto &i : v) {
+            hash ^= i + 0x9e3779b9 + (hash << 6) + (hash >> 2);
+        }
+        return hash;
+    }
+};
+
+ull solve(std::vector<int> &stair);
+int get_top_right(std::vector<int> &stair);
+int get_max_tiling_size(const int top_right, std::vector<int> &stair);
 
 int n, m;
-std::unordered_map<std::string, ull> dp;
+std::unordered_map<std::vector<int>, ull, vector_hasher> dp;
 
 int main() {
-    std::vector<int> stair;
+    std::vector<int> stair(n);
 
     std::cin >> n >> m;
 
+    int value = m;
     for (int i = 0; i < n; i++) {
-        int value = m;
         std::cin >> value;
+        if (value > m) {
+            value = m;
+        }
         stair.push_back(value);
     }
 
     if (n <= 0 || m <= 0 || stair.at(n - 1) <= 0) {
         std::cout << 0 << "\n";
     } else {
-        std::cout << solve(&stair) << "\n";
+        std::cout << solve(stair) << "\n";
     }
 
     return 0;
 }
 
-ull solve(std::vector<int> *stair) {
-    std::string state = generate_state(stair);
-
-    if (dp[state]) {
-        return dp[state];
+ull solve(std::vector<int> &stair) {
+    if (dp.find(stair) != dp.end()) {
+        return dp.at(stair);
     }
 
     for (int i = 0; i < n; i++) {
-        if (stair->at(i)) {
+        if (stair.at(i)) {
             break;
         } else if (i == n - 1) {
-            return dp[state] = 1;
+            return dp[stair] = 1;
         }
     }
 
     int top_right = get_top_right(stair);
     int tiling_size = get_max_tiling_size(top_right, stair);
+
+    std::vector<int> new_stair(n);
     for (int i = 1; i <= tiling_size; i++) {
-        std::vector<int> new_stair = copy_vector(stair);
+        memcpy(new_stair.data(), stair.data(), n * sizeof(int));
         for (int j = 0; j < i; j++) {
             new_stair.at(top_right + j) -= i;
         }
-        dp[state] += solve(&new_stair);
+        dp[stair] += solve(new_stair);
     }
 
-    return dp[state];
+    return dp.at(stair);
 }
 
-int get_top_right(std::vector<int> *stair) {
+int get_top_right(std::vector<int> &stair) {
     int top_right = 0;
 
     for (int i = 1; i < n; i++) {
-        if (stair->at(top_right) < stair->at(i)) {
+        if (stair.at(top_right) < stair.at(i)) {
             top_right = i;
         }
     }
@@ -81,28 +93,17 @@ int get_top_right(std::vector<int> *stair) {
     return top_right;
 }
 
-int get_max_tiling_size(int top_right, std::vector<int> *stair) {
+int get_max_tiling_size(const int top_right, std::vector<int> &stair) {
     int tiling_size = 1;
 
     for (int i = top_right + 1; i < n; i++) {
-        if (stair->at(i) == stair->at(top_right) &&
-            stair->at(i) == stair->at(i - 1)) {
+        if (stair.at(i) == stair.at(top_right) &&
+            stair.at(i) == stair.at(i - 1)) {
             tiling_size++;
         } else {
             break;
         }
     }
 
-    return std::min(stair->at(top_right), tiling_size);
-}
-
-std::vector<int> copy_vector(std::vector<int> *stair) {
-    std::vector<int> new_stair(*stair);
-    return new_stair;
-}
-
-std::string generate_state(std::vector<int> *stair) {
-    std::string state(stair->begin(), stair->end());
-
-    return state;
+    return std::min(stair.at(top_right), tiling_size);
 }
